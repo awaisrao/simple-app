85c6dd165556ea79d9eba33abc0a32202f666e9a
root <root@centos7.linuxvmimages.local>
2022-06-14 07:07:17 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,47 @@
+import os
+from flask import Flask
+from flaskext.mysql import MySQL      # For newer versions of flask-mysql 
+# from flask.ext.mysql import MySQL   # For older versions of flask-mysql
+app = Flask(__name__)
+
+mysql = MySQL()
+
+mysql_database_host = 'MYSQL_DATABASE_HOST' in os.environ and os.environ['MYSQL_DATABASE_HOST'] or  'localhost'
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Root@123'
+app.config['MYSQL_DATABASE_DB'] = 'student_db'
+app.config['MYSQL_DATABASE_HOST'] = mysql_database_host
+mysql.init_app(app)
+
+conn = mysql.connect()
+
+cursor = conn.cursor()
+
+#@app.route("/")
+#def main():
+#    return "Welcome!"
+
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
+
+@app.route("/")
+def read():
+    cursor.execute("SELECT * FROM students")
+    row = cursor.fetchone()
+    name = []
+    student_class = []
+    age = []
+    address = []
+    while row is not None:
+      name.append(row[0])
+      row = cursor.fetchone()
+
+    return ",".join(name)
+
+if __name__ == "__main__":
+    app.run()
+
+
